99d5c2cbd115da1793fbd31c1b806d03b2876b83
Vedran Bartonicek <vbartoni@gmail.com>
2015-03-13 09:58:05 +0200

    Fix for smtp sending

--- nodechecker/nodechecker/lib/python/nodechecker/notification/mailer.py
+++ nodechecker/nodechecker/lib/python/nodechecker/notification/mailer.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python2
 
 import smtplib
-# import os
-import sys
 import logging
 import email.mime.text
-import nodechecker.util
 
 ENV_HM_HOME_DIR = "OI_HEALTH_MONITORING_ROOT"
 MSG_DELIMITER = '-----------------------------------------------' \
                 '------------------'
+
+
+
+
+#MSG_DELIMITER = ''\
+#"  __________  ___   _____    __               ____  __                             _ __             _                            __  _ _____            __  _"\
+#" /_  __/ __ \/   | / ___/   / /_  ___  ____ _/ / /_/ /_     ____ ___  ____  ____  (_) /_____  _____(_)___  ____ _   ____  ____  / /_(_) __(_)________ _/ /_(_)___  ____"\
+#  / / / / / / /| | \__ \   / __ \/ _ \/ __ `/ / __/ __ \   / __ `__ \/ __ \/ __ \/ / __/ __ \/ ___/ / __ \/ __ `/  / __ \/ __ \/ __/ / /_/ / ___/ __ `/ __/ / __ \/ __ \"\
+#" / / / /_/ / ___ |___/ /  / / / /  __/ /_/ / / /_/ / / /  / / / / / / /_/ / / / / / /_/ /_/ / /  / / / / / /_/ /  / / / / /_/ / /_/ / __/ / /__/ /_/ / /_/ / /_/ / / / /"\
+#"_/  \____/_/  |_/____/  /_/ /_/\___/\__,_/_/\__/_/ /_/  /_/ /_/ /_/\____/_/ /_/_/\__/\____/_/  /_/_/ /_/\__, /  /_/ /_/\____/\__/_/_/ /_/\___/\__,_/\__/_/\____/_/ /_/"\
+#"                                                                                                        /____/                                                          "\
 
 
 class MailSender(object):
@@ -27,27 +35,13 @@ class MailSender(object):
             msg = email.mime.text.MIMEText(
                 self.format_email_message(notification_list))
             msg['Subject'] = self.conf.email_subject
-            msg['From'] = self.conf.email_from
+            #msg['From'] = self.conf.email_from
+            msg['From'] = ''
             msg['To'] = self.conf.email_to
-            s = smtplib.SMTP(self.conf.email_smpt_server)
-            s.sendmail(
-                self.conf.email_from, self.conf.email_to,
-                msg.as_string())
+            s = smtplib.SMTP(self.conf.email_smpt_server, self.conf.email_smpt_port)
+            s.login(self.conf.email_smtp_username, self.conf.email_smtp_password)
+            s.sendmail('', self.conf.email_to, msg.as_string())
             s.quit()
-                #self.move_sent_items(notification_list)
-            #except:
-            #    nodechecker.util.log_exception(sys.exc_info())
-
-    # def process_node_status_alerts(self, node_list, category):
-    #     notification_list = []
-    #     try:
-    #         for n in node_list:
-    #             new_notification = notification.Notification()
-    #             notification_list.append(
-    #                   new_notification.from_node(n, category))
-    #         self.send_notifications(notification_list)
-    #     except:
-    #         nodechecker.util.log_exception(sys.exc_info())
 
     def format_email_message(self, notification_list):
         # Message header
@@ -96,18 +90,3 @@ class MailSender(object):
             msg_body_items += body
         msg_body = "".join(msg_body_items)
         return msg_header + msg_sender_info + msg_body
-
-        # def move_sent_items(self, notification_list):
-        #     try:
-        #         for n in notification_list:
-        #             if n.file_path:
-        #                 os.rename(n.file_path, os.path.join(
-        #                       os.environ[ENV_HM_HOME_DIR],
-        #                       self.conf.notifications_home,
-        #                       self.conf.notifications_sent_dir,
-        #                       n.file_name))
-        #             #should log notification not originating from collect too
-        #             else:
-        #                 pass
-        #     except:
-        #         nodechecker.util.log_exception(sys.exc_info())
